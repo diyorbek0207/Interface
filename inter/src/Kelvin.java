eaf123444ea970a0b89c3dcd6f05138cbf6c3c2d
diyorbek0207 <140248878+diyorbek0207@users.noreply.github.com>
2023-12-08 16:55:34 +0500

    add Kelvin

--- inter/src/Kelvin.java
+++ inter/src/Kelvin.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Kelvin {
+public class Kelvin implements BaseConverter{
+    @Override
+    public void convert(double a) {
+        double K=a + 273.15;
+        System.out.println("Kelvin="+K);
+    }
+
+    public static void main(String[] args) {
+        Selsiy selsiy=new Selsiy();
+        Kelvin kelvin=new Kelvin();
+
+        kelvin.convert(selsiy.give());
+    }
 }
