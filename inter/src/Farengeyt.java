327d39fefcf76fb67f734ab5f05fd5b1367f558b
diyorbek0207 <140248878+diyorbek0207@users.noreply.github.com>
2023-12-08 16:51:24 +0500

    add Farengeyt

--- inter/src/Farengeyt.java
+++ inter/src/Farengeyt.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Farengeyt {
+public class Farengeyt implements BaseConverter{
+    @Override
+    public void convert(double a) {
+
+        double F=a*(9/5) +32;
+        System.out.println("Farengeyt="+F);
+    }
+
+    public static void main(String[] args) {
+        Selsiy selsiy=new Selsiy();
+        Farengeyt farengeyt=new Farengeyt();
+
+        farengeyt.convert(selsiy.give());
+    }
 }
