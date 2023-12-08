9fee4c34ddd8f00486a486192e96255f205a5190
diyorbek0207 <140248878+diyorbek0207@users.noreply.github.com>
2023-12-08 16:56:09 +0500

    Player

--- inter/src/Player.java
+++ inter/src/Player.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Player {
+public class Player implements Information{
+    private String nameOfPlayer;
+    private String sportTyp;
+    private int numberPasition;
+    private String teamName;
+    @Override
+    public void getinfo() {
+
+    }
+
+    @Override
+    public void getTeamNaem() {
+
+    }
+
+    public String getNameOfPlayer(){
+        return nameOfPlayer;
+    }
+    public String getSportTyp(){
+        return sportTyp;
+    }
+    public int getNumberPasition(){
+        return numberPasition;
+    }
+    public String getTeamName(){
+        return teamName;
+    }
 }
