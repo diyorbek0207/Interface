8d586514a4693221ef4ac16a46623bd2e22f22cb
diyorbek0207 <140248878+diyorbek0207@users.noreply.github.com>
2023-12-08 16:57:45 +0500

    add Selsiy

--- inter/src/Selsiy.java
+++ inter/src/Selsiy.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Selsiy {
+
+public class Selsiy implements BaseConverter{
+
+    private double gradus=45;
+    @Override
+    public void convert(double a) {
+        System.out.println("selsiy="+a);
+    }
+
+
+
+    public double give(){
+        //System.out.println(gradus);
+        return gradus;
+    }
+
+    public static void main(String[] args) {
+        Selsiy selsiy=new Selsiy();
+        selsiy.convert(selsiy.gradus);
+        selsiy.give();
+
+    }
 }
